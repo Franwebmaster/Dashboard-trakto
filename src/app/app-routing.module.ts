25245069fc5869c24fd7b0c0e632e40c110ff360
Francisco <franciscobarros754@gmail.com>
2019-01-20 19:37:17 -0200

    subindo estrutura projeto e environment base url API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+//App Routing
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'panel'
+  },
+
+  {
+    path: 'panel',
+    loadChildren: './sistema/panel/panel.module#PanelModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiBase: 'https://us-central1-trakto-dev.cloudfunctions.net/unsplashApi/api/v1/photos/'
 };
 
 /*
